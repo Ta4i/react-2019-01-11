04ecc4ab3c970f8fc6453574fa84ed439f75b2b4
tchmil <sergeytchmil@gmail.com>
2019-01-16 21:34:21 +0200

    init

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -5,8 +5,9 @@ export default (OriginalComponent) =>
     class DecoratedComponent extends Component {
         state = {
             openItemId: null
-        }
-        toggleOpenArticle = (id) => this.setState({openItemId: id})
+        };
+        toggleOpenArticle = (id) =>
+            (this.state.openItemId === id) ? this.setState({openItemId: null}) : this.setState({openItemId: id});
 
         render() {
             return <OriginalComponent
