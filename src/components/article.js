bc762789e1ed18510e03af903d5f8500db7df6ab
Oleg <super.oleg.3000@yandex.ru>
2019-01-16 20:20:59 +0300

    Add comment list to each article

--- src/components/article.js
+++ src/components/article.js
@@ -1,5 +1,14 @@
 import React, { PureComponent } from 'react';
 
+function Comment({ comment }) {
+    return (
+        <li>
+            <h5>{comment.user}</h5>
+            <span>{comment.text}</span>
+        </li>
+    )
+}
+
 class Article extends PureComponent {
     render() {
         const { article: { title }, isOpen } = this.props
@@ -14,6 +23,7 @@ class Article extends PureComponent {
                     </button>
                 </h3>
                 {this.body}
+                {this.comments}
             </div>
         )
     }
@@ -30,6 +40,14 @@ class Article extends PureComponent {
         if (!this.props.isOpen) return null
         return (
             <p>{this.props.article.text}</p>
+        )
+    }
+
+    get comments() {
+        const { article: { comments } } = this.props
+        if (!comments) return null
+        return comments.map(comment =>
+            <Comment key={comment.id} comment={comment} />
         )
     }
 }
