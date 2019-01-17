c0bf0258718d336ba728f86e1cede5d5c39194b2
Andrey Nikonorenkov <anikonorenkov@1stquad.com>
2019-01-17 12:22:50 +0300

    2. Подключить https://www.npmjs.com/package/react-datepicker и выводить диапазон дат;

--- src/app.js
+++ src/app.js
@@ -1,34 +1,68 @@
-import React, { Component } from 'react';
-import ArticleList from './components/article-list';
-import UserForm from './components/user-form';
-import Select from 'react-select';
+import React, { Component } from 'react'
+import ArticleList from './components/article-list'
+import UserForm from './components/user-form'
+import Select from 'react-select'
+import DatePicker from 'react-datepicker'
+
+import "react-datepicker/dist/react-datepicker.css";
 
 class App extends Component {
-    state = {
-        selected: null
-    }
+  state = {
+    selected: null,
+    startDate: null,
+    endDate: null
+  }
+
   render() {
     return (
       <div>
-          <UserForm/>
-          <Select
-              options={this.options}
-              value={this.state.selected}
-              onChange={this.handleSelectChange}
-          />
-          <ArticleList
-              articles={this.props.articles}
-          />
+        <DatePicker
+          selected={this.state.startDate}
+          selectsStart
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeStart}
+        />
+
+        <DatePicker
+          selected={this.state.endDate}
+          selectsEnd
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeEnd}
+        />
+        <br/>
+        Date range: {this.state.startDate ? this.state.startDate.toDateString() : ''} - {this.state.endDate ? this.state.endDate.toDateString(): ''}
+        <br/>
+        <br/>
+        <UserForm/>
+        <br/>
+        <br/>
+        <Select
+          options={this.options}
+          value={this.state.selected}
+          onChange={this.handleSelectChange}
+        />
+        <br/>
+        <ArticleList
+          articles={this.props.articles}
+        />
       </div>
-    );
+    )
   }
-    handleSelectChange = (selected) => this.setState({selected})
+
+  handleSelectChange = (selected) => this.setState({ selected })
+
+  handleChangeStart = (startDate) => this.setState({ startDate })
+
+  handleChangeEnd = (endDate) => this.setState({ endDate })
+
   get options() {
-      return this.props.articles.map(article => ({
-          value: article.id,
-          label: article.title
-      }))
+    return this.props.articles.map(article => ({
+      value: article.id,
+      label: article.title
+    }))
   }
 }
 
-export default App;
+export default App
