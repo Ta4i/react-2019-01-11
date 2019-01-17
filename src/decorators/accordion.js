e7bd0f07b0c037abcd9bcb5c27b8e7a393249e78
Dmitry Komlev <komlev_dmitry_1994@mail.ru>
2019-01-17 07:57:56 +0300

    Fixed button operation "open/close"

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -1,19 +1,25 @@
 // HOC - higher order component
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
 export default (OriginalComponent) =>
     class DecoratedComponent extends Component {
         state = {
             openItemId: null
         }
-        toggleOpenArticle = (id) => this.setState({openItemId: id})
 
-        render() {
-            return <OriginalComponent
-                {...this.props}
-                {...this.state}
-                toggleOpenArticle={this.toggleOpenArticle}
-            />;
+        toggleOpenArticle = (id) => {
+            this.setState({
+                openItemId: id === this.state.openItemId ? null : id
+            })
         }
 
+        render() {
+            return (
+                <OriginalComponent
+                    {...this.props}
+                    {...this.state}
+                    toggleOpenArticle={this.toggleOpenArticle}
+                />
+            )
+        }
     }
