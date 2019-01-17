8c64cf1e693f817ee8e4c2276f2a607f25056138
Alfiya <33545574+alfiya-udc@users.noreply.github.com>
2019-01-17 10:52:20 +0300

    Добавлен диапазон дат

--- src/app.js
+++ src/app.js
@@ -3,32 +3,40 @@ import ArticleList from './components/article-list';
 import UserForm from './components/user-form';
 import Select from 'react-select';
 
+import "react-datepicker/dist/react-datepicker.css";
+import DateRange from './components/date-range'
+
 class App extends Component {
     state = {
-        selected: null
+        selected: null        
     }
-  render() {
-    return (
-      <div>
-          <UserForm/>
-          <Select
-              options={this.options}
-              value={this.state.selected}
-              onChange={this.handleSelectChange}
-          />
-          <ArticleList
-              articles={this.props.articles}
-          />
-      </div>
-    );
-  }
-    handleSelectChange = (selected) => this.setState({selected})
-  get options() {
-      return this.props.articles.map(article => ({
-          value: article.id,
-          label: article.title
-      }))
-  }
+    render() {
+    
+        return (
+            <div>
+                <UserForm />
+                <Select
+                    options={this.options}
+                    value={this.state.selected}
+                    onChange={this.handleSelectChange}
+                />
+                <DateRange />
+                <ArticleList
+                    articles={this.props.articles}
+                />
+            </div>
+        );
+    }
+
+    handleSelectChange = (selected) => this.setState({ selected })    
+
+    get options() {
+        return this.props.articles.map(article => ({
+            value: article.id,
+            label: article.title
+        }))
+    }
+    
 }
 
 export default App;
