646b4070c8432bf35b753f8493f6a4714776b036
Sidorenko Anton <xmeranuxapbx@gmail.com>
2019-01-17 13:14:31 +0500

    Add multiselect

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ class App extends Component {
         <div>
             <UserForm/>
             <Select
+                isMulti
                 options={this.options}
                 value={this.state.selected}
                 onChange={this.handleSelectChange}
