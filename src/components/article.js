cd0a4e9f094e2a2ab68082c86520284fe11c9ba0
anastasiia peacefull <nastya.peacefull@mail.com>
2019-01-16 20:25:16 -0800

    HT #1, 4, 5

--- src/components/article.js
+++ src/components/article.js
@@ -1,4 +1,5 @@
 import React, {PureComponent} from 'react'
+import CommentList from './comment-list'
 
 class Article extends PureComponent {
     render() {
@@ -22,9 +23,14 @@ class Article extends PureComponent {
     }
 
     get body() {
-        if (!this.props.isOpen) return null
+        const { article, isOpen } = this.props
+        if (!isOpen) return null
+      
         return (
-            <p>{this.props.article.text}</p>
+            <div>
+              <p>{article.text}</p>
+              <CommentList comments={article.comments} />
+            </div>
         )
     }
 }
