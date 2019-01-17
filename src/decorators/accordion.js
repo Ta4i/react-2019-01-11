d1631af90909a224940820da0671db2f5e1247f9
Andrey Nikonorenkov <anikonorenkov@1stquad.com>
2019-01-17 11:51:03 +0300

    1. Починить закрытие статьи

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -6,7 +6,7 @@ export default (OriginalComponent) =>
         state = {
             openItemId: null
         }
-        toggleOpenArticle = (id) => this.setState({openItemId: id})
+        toggleOpenArticle = (id) => (this.state.openItemId === id)  ? this.setState({openItemId: null}) : this.setState({openItemId: id})
 
         render() {
             return <OriginalComponent
