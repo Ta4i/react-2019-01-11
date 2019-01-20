4a273e90d154c6a29fa10e78d319bba010491229
kraevskiy <ilya.kraevskiy@jointoit.com>
2019-01-20 12:42:39 +0200

    fix aniomation

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -24,7 +24,7 @@ class Article extends PureComponent {
                 <CSSTransition
                     transitionName="article"
                     transitionEnterTimeout={500}
-                    transitionLeaveTimeout={3000}
+                    transitionLeaveTimeout={500}
                 >
                     {this.body}
                 </CSSTransition>
