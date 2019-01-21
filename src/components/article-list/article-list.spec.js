fb477debf8ea23ac75cd545d92d98475f2760e09
t.voteva <t.voteva@qiwi.com>
2019-01-21 22:26:33 +0300

    Implement HW2

---
+++ src/components/article-list/article-list.spec.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Enzyme, { render, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import ArticleList from './index'
+import mockedArticles from '../../fixtures'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Article List', function() {
+    it('should render', () => {
+        const wrapper = mount(
+            <ArticleList articles={mockedArticles}/>
+        )
+
+        expect(wrapper.find('.test--art__container').length)
+            .toEqual(7)
+    })
+
+    it('should render without open articles', () => {
+        const wrapper = render(
+            <ArticleList articles={mockedArticles}/>
+        )
+
+        expect(wrapper.find('.test--article_body').length)
+            .toEqual(0)
+    })
+
+    it('should show article text after click on button', () => {
+        const wrapper = mount(
+            <ArticleList articles={mockedArticles}/>
+        )
+
+        wrapper.find('.test--article__btn').at(0).simulate('click')
+
+        expect(wrapper.find('.test--article_body').length)
+            .toEqual(1)
+    })
+
+    it('should hide article text after re-click on button', (done) => {
+        const wrapper = mount(
+            <ArticleList articles={[mockedArticles[0]]}/>
+        )
+
+        wrapper.find('.test--article__btn').at(0).simulate('click')
+        wrapper.find('.test--article__btn').at(0).simulate('click')
+
+        setTimeout(() => {
+            wrapper.update()
+
+            expect(wrapper.find('.test--article_body').length)
+                .toEqual(0)
+
+            done()
+
+        }, 1500)
+    })
+
+
+    it('should call fetch data on init', (done) => {
+        const wrapper = mount(
+            <ArticleList
+                articles={mockedArticles}
+                fetchData={() => done()}
+            />
+        )
+    })
+
+})
--- src/components/comment-list/index.js
+++ src/components/comment-list/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import Comment from './comment'
-import toggleOpen from '../decorators/toggleOpen'
-import PropTypes from 'prop-types';
+import Index from '../comment/index'
+import toggleOpen from '../../decorators/toggleOpen'
+import PropTypes from 'prop-types'
+import CSSTransition from 'react-addons-css-transition-group'
+import './comment-list.css'
 
 class CommentList extends Component {
     static propTypes = {
@@ -16,10 +18,16 @@ class CommentList extends Component {
         const { isOpen, toggleOpenItem } = this.props
         return (
             <div>
-                <button onClick={toggleOpenItem}>
+                <button onClick={toggleOpenItem} className='test--comments__btn'>
                     {isOpen ? 'hide comments' : 'show comments'}
                 </button>
-                {isOpen ? this.getBody() : null}
+                <CSSTransition
+                    transitionName="comment-list"
+                    transitionEnterTimeout={300}
+                    transitionLeaveTimeout={300}
+                >
+                    {isOpen ? this.getBody() : null}
+                </CSSTransition>
             </div>
         )
     }
@@ -29,8 +37,8 @@ class CommentList extends Component {
         const body = comments.length ? (
             <ul>
                 {comments.map((comment) => (
-                    <li key={comment.id}>
-                        <Comment comment={comment} />
+                    <li key={comment.id} className='test--comment__container'>
+                        <Index comment={comment}/>
                     </li>
                 ))}
             </ul>
@@ -39,6 +47,12 @@ class CommentList extends Component {
         )
         return <div>{body}</div>
     }
+}
+
+CommentList.propTypes = {
+    comments: PropTypes.array.isRequired,
+    isOpen: PropTypes.bool,
+    toggleOpenItem: PropTypes.func.isRequired
 }
 
 export default toggleOpen(CommentList)
