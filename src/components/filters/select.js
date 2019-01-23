ba0d34339a0b7edca908ccc07bb34bde6cd2e12b
Kraevskiy Illya <dsik-web@ukr.net>
2019-01-23 18:04:31 +0200

    test

--- src/components/filters/select.js
+++ src/components/filters/select.js
@@ -1,44 +1,44 @@
 import React, { Component } from 'react'
 import Select from 'react-select'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { selectArticle } from '../../ac'
 
 class SelectFilter extends Component {
-    state = {
-        selectedOption: null
-    }
+  state = {
+    selectedOption: null
+  }
 
-    render() {
-        return (
-            <Select
-                options={this.optionsForSelect}
-                onChange={this.handleSelectChange}
-                value={this.state.selectedOption}
-                isMulti
-            />
-        )
-    }
+  render() {
+    return (
+      <Select
+        options={this.optionsForSelect}
+        onChange={this.handleSelectChange}
+        value={this.state.selectedOption}
+        isMulti
+      />
+    )
+  }
 
-    get optionsForSelect() {
-        return this.props.articlesFromStore.map((item) => ({
-            value: item.id,
-            label: item.title
-        }))
-    }
+  get optionsForSelect() {
+    return this.props.articlesFromStore.map((item) => ({
+      value: item.id,
+      label: item.title
+    }))
+  }
 
-    handleSelectChange = (selectedOption) => {
-        this.setState({ selectedOption })
-        this.props.dispatchSelectArticle(selectedOption)
-    }
+  handleSelectChange = (selectedOption) => {
+    this.setState({ selectedOption })
+    this.props.dispatchSelectArticle(selectedOption)
+  }
 }
 
 const mapStateToProps = (store) => ({
-    articlesFromStore: store.articles
+  articlesFromStore: store.articles
 })
 
 export default connect(
   mapStateToProps,
   (dispatch) => ({
-      dispatchSelectArticle: (selectedOption) => dispatch(selectArticle(selectedOption))
+    dispatchSelectArticle: (selectedOption) => dispatch(selectArticle(selectedOption))
   })
 )(SelectFilter)
--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,15 +1,16 @@
-import articlesMock from '../fixtures';
-import {DELETE_ARTICLE, SELECT_ARTICLE} from '../constants';
+import articlesMock from '../fixtures'
+import { DELETE_ARTICLE, SELECT_ARTICLE } from '../constants'
 
 export default (articles = articlesMock, action) => {
-    const {type, payload} = action
-    switch (type) {
-        case DELETE_ARTICLE:
-            return articles.filter(article => article.id !== payload.id)
-        case SELECT_ARTICLE:
-            console.log(payload)
-            break;
-        default:
-            return articles
-    }
+  const { type, payload } = action
+  switch (type) {
+    case DELETE_ARTICLE:
+      return articles.filter(article => article.id !== payload.id)
+    case SELECT_ARTICLE:
+      console.log(payload)
+      return articles.filter(article => article.id !== payload.id)
+      break;
+    default:
+      return articles
+  }
 }
