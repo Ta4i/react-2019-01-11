6a6c9f66b8b5d57f65310c84840135e956fa1e64
Oleg <super.oleg.3000@yandex.ru>
2019-01-26 19:30:22 +0300

    Add comment submit form

--- src/components/user-form/user-form.js
+++ src/components/user-form/user-form.js
@@ -1,32 +1,52 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import './user-form.css'
 
 class UserForm extends Component {
-    state = {
+  state = {
+    user: ''
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <label>Article to add comment to:</label>
+        <select>{this.articleOptions}</select>
+        <label>Username:</label>
+        <input value={this.state.user} onChange={this.handleChange} />
+        <label>Comment body:</label>
+        <textarea />
+        <div className="button">
+          <button>Add comment</button>
+        </div>
+      </div>
+    )
+  }
+
+  get articleOptions() {
+    const { articles } = this.props
+    return articles.map((article) => (
+      <option key={article.id} value={article.id}>
+        {article.title}
+      </option>
+    ))
+  }
+
+  handleChange = (event) => {
+    event.preventDefault()
+
+    if (event.target.value.length > 10) {
+      return this.setState({
         user: ''
+      })
     }
 
-    render() {
-        return (
-            <div>
-                Username:
-                <input value={this.state.user} onChange={this.handleChange}/>
-            </div>
-        )
-    }
-
-    handleChange = (event) => {
-        event.preventDefault()
-
-        if (event.target.value.length > 10) {
-            return this.setState({
-                user: ''
-            })
-        }
-
-        this.setState({
-            user: event.target.value
-        })
-    }
+    this.setState({
+      user: event.target.value
+    })
+  }
 }
 
-export default UserForm
+export default connect((store) => ({
+  articles: store.articles
+}))(UserForm)
