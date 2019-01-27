58a31ada757a618e51cc1f83931e82f923a6baad
Oleg <super.oleg.3000@yandex.ru>
2019-01-27 11:53:14 +0300

    Fix middleware (pass all actions through)

--- src/middleware/idGenerator.js
+++ src/middleware/idGenerator.js
@@ -4,6 +4,6 @@ import { CREATE_COMMENT } from '../constants'
 export default (store) => (next) => (action) => {
   if (action.type === CREATE_COMMENT) {
     action.payload.comment.id = uuidv1()
-    next(action)
   }
+  next(action)
 }
