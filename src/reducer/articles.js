45a78e76f3984a750dd2902aa51a3a8c487f9ead
Oleg <mightyxerox@gmail.com>
2019-01-27 20:06:51 +0300

    Change default articles to an object

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,11 +1,32 @@
-import {normalizedArticles} from '../fixtures';
-import {DELETE_ARTICLE} from '../constants';
+import { normalizedArticles } from '../fixtures';
+import { DELETE_ARTICLE, APPEND_COMMENT } from '../constants';
 
-export default (articles = normalizedArticles, action) => {
-    const {type, payload} = action
+const defaultArticles = normalizedArticles.reduce((acc, article) => ({
+    ...acc,
+    [article.id]: article
+}), {})
+
+export default (articles = defaultArticles, action) => {
+    const { type, payload } = action
+    let updatedArticles = null
     switch (type) {
         case DELETE_ARTICLE:
-            return articles.filter(article => article.id !== payload.id)
+            updatedArticles = { ...articles }
+            delete updatedArticles[payload.id]
+            return updatedArticles
+        case APPEND_COMMENT:
+            const { articleId, commentId } = payload
+            updatedArticles = { ...articles }
+            const article = updatedArticles[articleId]
+            let comments = null
+            if (article.comments) {
+                comments = [...article.comments, commentId]
+            } else {
+                comments = [commentId]
+            }
+            const updatedArticle = { ...article, comments: comments }
+            updatedArticles[articleId] = updatedArticle
+            return updatedArticles
         default:
             return articles
     }
